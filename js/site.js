551950d5ce91f5f6b12367f6468ee8fc0caacb5b
Paul Sanwald <paul@redowlanalytics.com>
2012-11-23 17:52:50 -0500

    turning off autoscrolling, splitting out css and javascript

---
+++ js/site.js
@@ -0,0 +1,9 @@
+/* Custom javascript for Sanwald Woodworking */
+
+$(function() {
+    $('.carousel').each(function(){
+        $(this).carousel({
+            interval: false
+        });
+    });
+});
